beb892d699cbb80745af03e73213d866f7726acd
Albert Wang <albertywang@gmail.com>
2012-08-16 22:56:46 -0500

    Templating

--- public/js/fourm.ui.js
+++ public/js/fourm.ui.js
@@ -1,78 +1,94 @@
-(function()
+
+//Some UI utility methods, for creating scrollbars and such things.
+(function ()
 {
 	var INITIAL_OFFSET = 50;
-	var SCROLL_AMOUNT = 40;
+	var SCROLL_AMOUNT = 250;
 
 	function createScrollable(id)
 	{
 		var main = $("#" + id + " > .scrollable");
-    var bar  = $("#" + id + " > .scrollbar");
+	    var bar  = $("#" + id + " > .scrollbar");
 
-    /*
-     * Bar position = (position / total) * (total - barsize)
-     * barsize / scrollsize = pagesize / totalsize;
-     *  -> pagesize / totalsize * scrollsize;
-     */
-    
-    function resetBar()
+	    /*
+		* Bar position = (position / total) * (total - barsize)
+		* barsize / scrollsize = pagesize / totalsize;
+		*  -> pagesize / totalsize * scrollsize;
+		*/
+	
+	    function resetBar()
+	    {
+		    var min = 0;
+		    var max = main.height() - $(document).height() + INITIAL_OFFSET;
+
+		    var size = ($(document).height() - INITIAL_OFFSET) * ($(document).height() - INITIAL_OFFSET) / main.height();
+		    bar.css("height", size + "px");
+
+		    var off = main.data("top") || 0;
+		    off = off / max * ($(document).height() - size - INITIAL_OFFSET);
+		    off = off + INITIAL_OFFSET;
+
+		    bar.css("top", off + "px");
+	    }
+
+	    $(window).resize(resetBar);
+
+	    main.bind("mousewheel", function(event)
+	    {
+	        bar.css("opacity", 1.0);
+	        bar.show();
+
+	        var position = $(main).data("top") || 0;
+	        var delta = event.originalEvent.wheelDelta;
+
+	        if (delta >= 0) {
+	            position -= SCROLL_AMOUNT;
+	        }
+	        else {
+	            position += SCROLL_AMOUNT;
+	        }
+
+	        if (position < 0) {
+	            position = 0;
+	        }
+
+	        if (position > (main.height() - $(document).height() + INITIAL_OFFSET)) {
+	            position = (main.height() - $(document).height() + INITIAL_OFFSET);
+	        }
+
+	        $(main).data("top", position);
+	        $(main).css("top", -position + INITIAL_OFFSET);
+
+	        resetBar();
+
+	        bar.stop(true);
+	        bar.delay(250).fadeOut();
+	    });
+
+	    resetBar();
+	}
+
+	var fourm = {};
+	fourm.ui =
     {
-      var min = 0;
-      var max = main.height() - $(document).height() + INITIAL_OFFSET;
-
-      var size = ($(document).height() - INITIAL_OFFSET) * ($(document).height() - INITIAL_OFFSET) / main.height();
-      bar.css("height", size + "px");
-
-      var off = main.data("top") || 0;
-      off = off / max * ($(document).height() - size - INITIAL_OFFSET);
-      off = off + INITIAL_OFFSET;
-
-      bar.css("top", off + "px");
+		createScrollable: createScrollable
     }
 
-    $(window).resize(resetBar);
-
-		main.bind("mousewheel", function(event)
-		{
-      bar.css("opacity", 1.0);
-      bar.show();
-
-			var position = $(main).data("top") || 0;
-			var delta = event.originalEvent.wheelDelta;
-
-			if (delta >= 0)
-			{
-				position -= SCROLL_AMOUNT;
-			} 
-			else 
-			{
-				position += SCROLL_AMOUNT;
-			}
-
-			if (position < 0)
-			{
-				position = 0;
-			}
-
-			if (position > (main.height() - $(document).height() + INITIAL_OFFSET))
-			{
-				position = (main.height() - $(document).height() + INITIAL_OFFSET);
-			}
-
-			$(main).data("top", position);
-			$(main).css("top", -position + INITIAL_OFFSET);
-
-      resetBar();
-
-      bar.stop(true);
-      bar.delay(250).fadeOut();
-		});
-
-    resetBar();
-	}
-
-	fourm = {};
-	fourm.ui = {
-		createScrollable: createScrollable
-	}
-
+	window.fourm = fourm;
 }());
+
+//Templates
+(function ()
+{
+	var navigationTemplate =
+		"{{#titles}}" + 
+			"<a href='#'><li>" +
+				"<div class='title'><div class='avatar'><img src='http://2.gravatar.com/avatar/0d8d5485c71164e89c41f0cc22d8cc10'/></div>" +
+				"<p>{{title}}<span class='extra'>&nbsp;{{extra}}</span></p><span class='count'>{{count}}</span></div><div class='clearboth'></div>" +
+			"</li></a>" + 
+		"{{/titles}}"
+
+	window.fourm.templates = {
+		navigation: navigationTemplate
+	}
+}())
